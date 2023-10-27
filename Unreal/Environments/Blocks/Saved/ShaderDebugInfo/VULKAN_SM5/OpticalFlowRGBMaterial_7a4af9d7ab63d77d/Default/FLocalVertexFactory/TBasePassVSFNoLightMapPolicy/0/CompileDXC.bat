cd2ee7a83acd43f0f84fad908936cbec0b5f4087
Mark B <no@way.com>
2023-10-27 18:21:42 +0300

    Content

---
+++ Unreal/Environments/Blocks/Saved/ShaderDebugInfo/VULKAN_SM5/OpticalFlowRGBMaterial_7a4af9d7ab63d77d/Default/FLocalVertexFactory/TBasePassVSFNoLightMapPolicy/0/CompileDXC.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+SET DXC="/home/someone/AirSim/UE_5.2.0/Engine/Binaries/ThirdParty/ShaderConductor/Win64/dxc.exe"
+SET SPIRVCROSS="spirv-cross.exe"
+IF NOT EXIST %DXC% (
+	ECHO Couldn't find dxc.exe under "/home/someone/AirSim/UE_5.2.0/Engine/Binaries/ThirdParty/ShaderConductor/Win64"
+	GOTO :END
+)
+ECHO Compiling with DXC...
+%DXC% -HV 2018 -T vs_6_2 -E Main -spirv -fspv-target-env=vulkan1.1 -Fo BasePassVertexShader.DXC.spv BasePassVertexShader.usf
+WHERE %SPIRVCROSS%
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO spirv-cross.exe not found in Path environment variable, please build it from source https://github.com/KhronosGroup/SPIRV-Cross
+	GOTO :END
+)
+ECHO Translating SPIRV back to glsl...
+%SPIRVCROSS% --vulkan-semantics --output BasePassVertexShader.SPV.glsl BasePassVertexShader.DXC.spv
+:END
+PAUSE
