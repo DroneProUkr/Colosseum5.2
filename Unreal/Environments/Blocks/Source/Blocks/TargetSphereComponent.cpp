bcb086a297eb67e8388603303f7dbb3597c98e47
Mark B <no@way.com>
2023-11-03 18:29:48 +0200

    Movement script

--- Unreal/Environments/Blocks/Source/Blocks/TargetSphereComponent.cpp
+++ Unreal/Environments/Blocks/Source/Blocks/TargetSphereComponent.cpp
@@ -9,7 +9,7 @@ const int WP_NUMBER = 5;
 const float HEIGHT = 10.0f;
 const float m_to_cm = 100.0f;
 const float cm_to_m = 0.01f;
-const FVector WP[WP_NUMBER] =
+FVector WP[WP_NUMBER] =
 {
 	{500, -500, HEIGHT},
 	{500, -100, HEIGHT},
@@ -18,7 +18,7 @@ const FVector WP[WP_NUMBER] =
 	{-500, -500, HEIGHT}
 };
 
-const float SPEED_MS = 90.0f / 3.6f;
+const float SPEED_MS = 180.0f / 3.6f;
 const float R_M = 10.0f;
 
 int nextWPIndex(int index)
@@ -55,7 +55,14 @@ void UTargetSphereComponent::SetPosition(FVector position)
 
 void UTargetSphereComponent::ResetPosition()
 {
-	m_currentWP = 0;
+	/*WP[0] = FVector(FMath::RandRange(1000.0f, 2000.0f), FMath::RandRange(-1000.0f, 1000.0f), FMath::RandRange(200.0f, 250.0f));
+	WP[1] = FVector(FMath::RandRange(-1000.0f, -2000.0f), FMath::RandRange(-1000.0f, 1000.0f), FMath::RandRange(200.0f, 250.0f));*/
+	WP[0] = FVector(2000.0f, FMath::RandRange(-1000.0f, 1000.0f), FMath::RandRange(200.0f, 250.0f));
+	WP[1] = FVector(-1000.0f, FMath::RandRange(-1000.0f, 1000.0f), FMath::RandRange(200.0f, 250.0f));
+	m_flightDirection = (WP[1] - WP[0]).GetUnsafeNormal();
+	SetPosition(WP[0]);
+
+	/*m_currentWP = 0;
 	m_flightDirection = (WP[nextWPIndex(m_currentWP)] - WP[m_currentWP]).GetUnsafeNormal();
 	//SetPosition(WP[m_currentWP]);
 
@@ -66,7 +73,7 @@ void UTargetSphereComponent::ResetPosition()
 			SetPosition(ActorItr->GetActorLocation() * cm_to_m);
 			break;
         }
-    }
+    }*/
 }
 
 void UTargetSphereComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -74,6 +81,17 @@ void UTargetSphereComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	auto position = GetOwner()->GetActorLocation() * cm_to_m;
+
+	if ((FVector::Dist(position, WP[0]) - FVector::Dist(WP[1], WP[0])) >= R_M)
+	{
+		ResetPosition();
+	}
+	else
+	{
+		SetPosition(position + m_flightDirection * SPEED_MS * DeltaTime);
+	}
+
+	/*auto position = GetOwner()->GetActorLocation() * cm_to_m;
 	auto nextPosition = WP[nextWPIndex(m_currentWP)];
 
 	if ((FVector::Dist(position, WP[m_currentWP]) - FVector::Dist(nextPosition, WP[m_currentWP])) >= R_M)
@@ -82,5 +100,5 @@ void UTargetSphereComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 		m_flightDirection = (WP[nextWPIndex(m_currentWP)] - position).GetUnsafeNormal();
 	}
 
-	SetPosition(position + m_flightDirection * SPEED_MS * DeltaTime);
+	SetPosition(position + m_flightDirection * SPEED_MS * DeltaTime);*/
 }
